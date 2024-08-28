33296f9761b1bbfaad02cc67c696472d8e13785d
Matheus <matheusgcresci@gmail.com>
2024-08-28 12:52:51 -0300

    final aula crud

--- editar-usuario.php
+++ editar-usuario.php
@@ -7,22 +7,23 @@ $row = $res->fetch_object();
 ?>
 
 <form action="?page=salvar" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
+    <input type="hidden" name="acao" value="editar">
+    <input type="hidden" name="id" value="<?php echo $row->id; ?>">
     <div class="mb-3">
         <label>Nome</label>
-        <input type="text" name="nome" class="form-control">
+        <input type="text" name="nome" value="<?php echo $row->nome; ?>"  class="form-control">
     </div>
     <div class="mb-3">
 <label>E-mail</label>
-<input type="email" name="email" class="form-control">
+<input type="email" name="email" value="<?php echo $row->email; ?>" class="form-control">
     </div>
     <div class="mb-3">
 <label>Senha</label>
-<input type="password" name="senha" class="form-control">
+<input type="password" name="senha"  class="form-control" required>
     </div>
     <div class="mb-3">
 <label>Data de Nascimento</label>
-<input type="date" name="data_nasc" class="form-control">
+<input type="date" name="data_nasc" value="<?php echo $row->data_nasc; ?>" class="form-control">
     </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary">Enviar</button>
--- salvar-usuario.php
+++ salvar-usuario.php
@@ -1,7 +1,8 @@
 <?php
 
 switch ($_REQUEST["acao"]) {
-    case 'cadastrar':
+
+    case 'cadastrar': //CADASTRAR
         $nome = $_POST["nome"];
         $email = $_POST["email"];
         $senha = md5($_POST["senha"]);
@@ -19,10 +20,46 @@ switch ($_REQUEST["acao"]) {
             echo "<script>location.href='?page=listar'</script>";
         }
         break;
-    case 'editar':
-        //code ..
+
+    case 'editar': //EDITAR
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = md5($_POST["senha"]);
+        $data_nasc = $_POST["data_nasc"];
+
+        $sql = "UPDATE usuarios SET
+        nome='{$nome}',
+        email='{$email}',
+        senha='{$senha}',
+        data_nasc='{$data_nasc}'
+        WHERE
+        id=".$_REQUEST["id"];        
+
+        $res = $conn->query($sql);
+
+        if ($res == true) {
+            echo "<script>alert('Editado com sucesso');</script>";
+            echo "<script>location.href='?page=listar'</script>";
+        } else {
+            echo "<script>alert('Não foi possível editar');</script>";
+            echo "<script>location.href='?page=listar'</script>";
+        }
         break;
-    case 'excluir':
-        //code ..
+
+    case 'excluir': //EXCLUIR
+        
+        
+        $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+
+        $res = $conn->query($sql);
+
+        if ($res == true) {
+            echo "<script>alert('Excluído com sucesso');</script>";
+            echo "<script>location.href='?page=listar'</script>";
+        } else {
+            echo "<script>alert('Não foi possível excluir');</script>";
+            echo "<script>location.href='?page=listar'</script>";
+        }
+
         break;
 }
